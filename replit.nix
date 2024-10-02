ef0a6e1fe6582fc3ce289464f560967d8da5d037
Raúl Raja Martínez <raulraja@gmail.com>
2024-10-02 10:00:15 +0000

    cors config

--- replit.nix
+++ replit.nix
@@ -3,4 +3,8 @@
         pkgs.corretto17
         pkgs.maven
     ];
+    environment.sessionVariables = rec {
+        ACCESS_CONTROL_ALLOW_ORIGIN_VALUE =	"*";
+        ACCESS_CONTROL_ALLOW_HEADER_VALUE =	"*";
+    };
 }
